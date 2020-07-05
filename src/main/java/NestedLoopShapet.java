f7c99e293b2f60de0824e163cd54d4aac8bac368
willhuang0204 <william_mj_huang@hotmail.com>
2020-07-05 17:03:34 +0800

    July 4 Learning

--- src/main/java/NestedLoopShapet.java
+++ src/main/java/NestedLoopShapet.java
@@ -1,10 +1,21 @@
 public class NestedLoopShapet {
     public static void main(String[] args) {
-        for (int i = 9; i>=0; i--){
-            for (int k = i; k>=0; k--){
+        for (int i = 0; i<=9; i++){
+            for (int k = 9-i; k<=8; k++){
                 System.out.print("* ");
             }
             System.out.println();
+        }
+
+        int f = 9;
+        while(f>=0){
+            int s =f;
+            while(s>=0) {
+                System.out.print("* ");
+                s--;
+            }
+            System.out.println();
+            f--;
         }
     }
 }
