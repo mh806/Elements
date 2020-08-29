9b38233a6fbcc23821f0b6e29b62b9e5b71969d7
willhuang0204 <william_mj_huang@hotmail.com>
2020-08-30 00:33:11 +0800

    29 Learning Data Structure

---
+++ src/main/java/Algorithm.java
@@ -0,0 +1,16 @@
+public class Algorithm {
+    //Algorithm and Data Structure
+    //time complexity -> Array O(n) -> return max number
+    //Algorithm classification
+    //constant O(1) -> grab specific data
+    //Infectorial O(n2) , O(2n)
+
+    //Constant Example
+    //HashTable ->
+    //5:William -> Hash Function -> Stored in Index
+    // [3] -> function -> found the data
+
+    //Memory address
+    //grade[3] =
+
+}
